c51bd547463fabd65b19b7d534c2caf3dbcc1004
awheeler294 <awheeler294@gmail.com>
2023-12-01 11:50:57 -0700

    parse command line args with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,42 @@
 use std::env;
 use std::fs;
 
+use clap::Parser;
+
 mod grid;
 mod y2015;
 mod y2022;
 mod y2023;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    year: String,
+    day: String,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if let Some(year) = args.get(1) {
-        if let Some(day) = args.get(2) {
-            println!("year {}", year);
-            println!("day {}", day);
+    println!("year {}", args.year);
+    println!("day {}", args.day);
 
-            let path = format!("input/{year}/day{day}");
-            let data = fs::read_to_string(&path).expect(&format!("Cant read {}", &path));
-            let lines = data.trim_end().split('\n').collect::<Vec<&str>>();
+    let path = format!("input/{}/day{}", args.year, args.day);
+    let data = fs::read_to_string(&path).expect(&format!("Cant read {}", &path));
+    let lines = data.trim_end().split('\n').collect::<Vec<&str>>();
 
-            match year.as_str() {
-                "2015" => {
-                    println!("{}", y2015::run_day(day, &lines))
-                }
-                "2022" => {
-                    println!("{}", y2022::run_day(day, &lines))
-                }
-                "2023" => {
-                    println!("{}", y2023::run_day(day, &lines))
-                }
-                _ => {
-                    println!("Unknown year {}", year);
-                }
-            }
-        } else {
-            println!("Usage: year day");
+    match args.year.as_str() {
+        "2015" => {
+            println!("{}", y2015::run_day(&args.day, &lines))
         }
-    } else {
-        println!("Usage: year day");
+        "2022" => {
+            println!("{}", y2022::run_day(&args.day, &lines))
+        }
+        "2023" => {
+            println!("{}", y2023::run_day(&args.day, &lines))
+        }
+        _ => {
+            println!("Unknown year {}", args.year);
+        }
     }
 }
