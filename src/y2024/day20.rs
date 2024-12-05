c595e49a291587b40f3e914cb7e8efbca0c985b1
Andrew Wheeler <awheeler294@gmail.com>
2024-12-05 11:07:13 -0700

    more useful day template

--- src/y2024/day20.rs
+++ src/y2024/day20.rs
@@ -1,18 +1,43 @@
-pub fn solve(_input: &[&str]) -> String {
-    let part1 = "";
-    let part2 = "";
+pub fn solve(input: &[&str]) -> String {
+   let part1 = solve_part_1(input);
+   let part2 = solve_part_2(input);
 
-    format!(" Part1: {} \n Part2: {}", part1, part2)
+   format!(" Part1: {} \n Part2: {}", part1, part2)
+}
+
+fn solve_part_1(input: &[&str]) -> usize {
+   input.len()
+}
+
+fn solve_part_2(input: &[&str]) -> usize {
+   input.len()
 }
 
 #[cfg(test)]
 mod tests {
     
-    // use super::*;
-    // use pretty_assertions::assert_eq;
+   use super::*;
+   use pretty_assertions::assert_eq;
 
-    #[test]
-    fn test_solve() {
+   #[test]
+   fn test_solve_part_1() {
+      let input = [];
 
-    }
+      let expected = 0;
+
+      let actual = solve_part_1(&input);
+
+      assert_eq!(actual, expected);
+   }
+
+   #[test]
+   fn test_solve_part_2() {
+      let input = [];
+
+      let expected = 0;
+
+      let actual = solve_part_2(&input);
+
+      assert_eq!(actual, expected);
+   }
 }
