394c4d16ebc1df94b3596d57a1574ce65ce2474e
Andrew Wheeler <awheeler294@gmail.com>
2025-12-07 09:07:59 -0700

    2025 day 7

--- src/y2025/day7.rs
+++ src/y2025/day7.rs
@@ -1,16 +1,52 @@
+use std::collections::HashSet;
+
+use crate::grid::Grid;
+
 pub fn solve(input: &[&str]) -> String {
-    let part1 = solve_part_1(input);
-    let part2 = solve_part_2(input);
+    let (part_1, part_2) = simulate_beams(input);
 
-    format!(" Part1: {} \n Part2: {}", part1, part2)
+    format!(" Part1: {part_1} \n Part2: {part_2}")
 }
 
-fn solve_part_1(input: &[&str]) -> usize {
-    input.len()
-}
+fn simulate_beams(input: &[&str]) -> (u64, u64) {
+    let grid = Grid::parse_char(input);
 
-fn solve_part_2(input: &[&str]) -> usize {
-    input.len()
+    let mut beams = HashSet::new();
+    let mut paths = vec![0; grid.width];
+    let mut splits = 0;
+    
+    for x in 0..grid.width {
+        if grid[(x, 0)] == 'S' {
+            beams.insert(x);
+            paths[x] += 1;
+            break;
+        }
+    }
+
+    for y in 2..grid.height {
+
+        let mut next_gen = HashSet::new();
+        let mut next_paths = vec![0; grid.width];
+
+        for x in beams {
+            if grid[(x, y)] == '^' {
+                splits += 1;
+                next_paths[x - 1] += paths[x];
+                next_paths[x + 1] += paths[x];
+                next_gen.insert(x - 1);
+                next_gen.insert(x + 1);
+            } else {
+                next_gen.insert(x);
+                next_paths[x] += paths[x];
+            }
+        }
+
+        beams = next_gen;
+        paths = next_paths;
+    }
+
+    (splits, paths.iter().sum())
+
 }
 
 #[cfg(test)]
@@ -19,12 +55,37 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
 
+    fn solve_part_1(input: &[&str]) -> u64 {
+        simulate_beams(input).0
+    }
+
+    fn solve_part_2(input: &[&str]) -> u64 {
+        simulate_beams(input).1
+    }
+
     #[test]
     fn test_solve_part_1() {
         #[rustfmt::skip]
-      let input = [];
+      let input = [
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "...............",
+        ];
 
-        let expected = 0;
+        let expected = 21;
 
         let actual = solve_part_1(&input);
 
@@ -34,9 +95,26 @@ mod tests {
     #[test]
     fn test_solve_part_2() {
         #[rustfmt::skip]
-      let input = [];
+      let input = [
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "...............",
+        ];
 
-        let expected = 0;
+        let expected = 40;
 
         let actual = solve_part_2(&input);
 
